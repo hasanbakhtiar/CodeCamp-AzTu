0ccfb5e7947c5afd0bf2cd424d7bb13073c81e8e
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-24 23:48:08 +0400

    lastest

---
+++ JavaScript/PureJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,40 @@
+// Primitive DT
+// string
+// var infoOne = "Hello";
+// number
+// var infoTwo = 5;
+// boolean (true | false)
+// var infoThree = true;
+
+// Composite DT 
+// Array 
+// var infoFive = [5,"hello",true];
+// Object 
+// var infoSix = {
+//     title: "BMW",
+//     speed: 200,
+//     newCar:false
+// }
+
+// Special DT 
+// undefined
+// var infoSeven;
+// null
+// var infoE = null;
+
+
+
+// debugger;
+// caonsolae.log("Hello");
+
+
+
+
+// var info = 5; //number
+// console.log(info);
+
+// console.log("Hello");
+// document.write('Hello1');
+
+
+
---
+++ JavaScript/PureJS/lesson3-operators/js/script.js
@@ -0,0 +1,113 @@
+// Assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+
+
+// arithmetic operators
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// var info = 5;
+// var infoData = 10;
+// var result = info + infoData;
+// result
+// console.log(result);
+
+
+
+// comparative operators
+// ==
+// ===
+// <
+// <=
+// >
+// >=
+// !=
+// !==
+
+// if (5 !== "5") {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+
+
+
+// logical operators
+// && - and
+// || - or
+// ! - not
+
+
+
+// string operators
+// +
+// +=
+
+// var a = "JavaScript";
+// var b = "Hello, ";
+// b+=a;
+// console.log(b);
+
+// Special Operators
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,      (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+// delete       (objectin dəyərini silmək üçündür)
+
+
+// var a  = 'JavaScript\'s books'
+// console.log(a);
+// var a = 5, b = 10;
+// console.log(a,b);
+// const a = [];
+// var c = typeof a;
+// console.log(c);
+
+
+// ternary
+// 5>10 ? console.log(true):console.log(false);
+// var a = 5;
+// console.log(void(a));
+
+// var a = {
+//     title:"Hello"
+// };
+// delete a.title
+// console.log(a.title);
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
