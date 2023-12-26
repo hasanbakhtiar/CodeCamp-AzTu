58c535d2b3c8a7dfd9343ed46327afcf9816cf0c
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-26 13:00:14 +0400

    condition

---
+++ JavaScript/PureJS/lesson4-variables-condition/js/script.js
@@ -0,0 +1,104 @@
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdən sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable (global)
+// const    -> constant (local)
+// let      -> let      (local)
+
+// let info = 5;
+// info = 10;
+// console.log(info);
+
+// ===============================================================================
+
+
+
+// if - eger
+// else - deyilse
+// else if - deyilse eger
+
+// if (5>10) {
+//     console.log('bu ifade duzdur');
+// }else if( 100>123 ){
+//     console.log('bu 123dur');
+// }
+// else if( "hello" == 5 ){
+//     console.log('bu hellodur');
+// }
+// else{
+//     console.log('bu ifade yanlisdir');
+// }
+
+
+
+// const time = 8;
+
+// if (time >=5 && time <= 12) {
+//     console.log('good morning');
+//     if (time === 8) {
+//         console.log('get up');
+//     }
+// }else if( time >= 13 && time <=17){
+//     console.log('good afternoon');
+// }
+// else if( time >= 18 && time <=21){
+//     console.log('good evening');
+// }
+// else if( time >= 21 && time <=23){
+//     console.log('good night');
+// }
+// else{
+//     console.log('no time');
+// }
+
+
+// const info = 10;
+
+// switch (info) {
+//     case 10:
+//         console.log('enter');
+//        if (info>5) {
+//         console.log('this is 10');
+//        }
+//         break;
+//     case 20:
+//         console.log('this is 20');
+//         break;
+//     default:
+//         console.log('this is default');
+//         break;
+// }
+
+
+
+
+
+
+
+
+
