b5a593b15f043df1c815177a9348f3ee6912102c
HasanBakhtiar <hasan.doctype@gmail.com>
2023-12-29 13:09:34 +0400

    lastest

---
+++ JavaScript/PureJS/lesson5-loops/js/script.js
@@ -0,0 +1,36 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// let a = 0; // variable
+// let c = 5;
+
+// while((a<10)&&(c==5)){ //condition
+//         a++;//action
+//         console.log("Hello"+a); //result
+//     }
+    
+
+// do{
+//     a++; //action
+//     console.log(a); //result
+// }while(a>10) //condition
+
+
+for(let a = 0; a<10;a++){
+    if (a ==5) {
+        continue;
+    }
+    console.log(a);
+}
+
+
+
+// const a = prompt();
+
+// console.log(typeof Number(a));
+
+
+
