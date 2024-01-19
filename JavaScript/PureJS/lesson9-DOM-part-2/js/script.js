4bdb2ed106ded2872a9131dd1e72c8cdb36eca33
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-19 12:14:49 +0400

    DOM part 2

---
+++ JavaScript/PureJS/lesson9-DOM-part-2/js/script.js
@@ -0,0 +1,49 @@
+// const text = document.querySelector('h1');
+// const btn = document.querySelector('button');
+// let sWitch = true;
+
+// btn.onclick = ()=>{
+//    if (sWitch) {
+//     text.style.backgroundColor = 'green';
+//     text.style.transition = '.5s';
+//     sWitch = false;
+//    }else{
+//     text.style.backgroundColor = '#fff';
+//     text.style.transition = '.5s';
+//     sWitch = true;
+
+//    }
+// }
+// ==============================================
+
+// const text = document.querySelector('h1');
+// const btn = document.querySelector('button');
+
+
+
+// btn.onclick =()=>{
+//     if(text.className == "colorFull"){
+//         text.className = "monoColor";
+//     }else{
+//         text.className = "colorFull";
+//     }
+// }
+
+
+
+// ==============================================
+
+const photo = document.querySelector('img');
+const btn = document.querySelector('button');
+
+
+btn.onclick = ()=>{
+    photo.attributes[1].value = "https://wallup.net/wp-content/uploads/2016/05/24/304149-nature.jpg"
+}
+
+
+
+
+
+
+
