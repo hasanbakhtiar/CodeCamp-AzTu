b1548f7285c1b274f1188c7f58f28ed30d801460
HasanBakhtiar <hasan.doctype@gmail.com>
2024-01-19 13:02:26 +0400

    DOM part 2

--- JavaScript/PureJS/lesson9-DOM-part-2/js/script.js
+++ JavaScript/PureJS/lesson9-DOM-part-2/js/script.js
@@ -33,14 +33,60 @@
 
 // ==============================================
 
-const photo = document.querySelector('img');
-const btn = document.querySelector('button');
+// const photo = document.querySelectorAll('img');
+const text = document.querySelector('h1');
+const btn = document.querySelectorAll('button')[1];
 
 
+const mode = document.querySelector('#mode');
 btn.onclick = ()=>{
-    photo.attributes[1].value = "https://wallup.net/wp-content/uploads/2016/05/24/304149-nature.jpg"
+    if (mode.className == 'light') {
+        mode.className = 'dark'
+        document.querySelector('nav').className = "navbar navbar-expand-lg bg-dark navbar-dark"
+        btn.innerHTML = "Light"
+        
+    }else{
+        mode.className = 'light'
+        btn.innerHTML = "Dark"
+        document.querySelector('nav').className = "navbar navbar-expand-lg bg-light navbar-light"
+
+    }
+        // text.classList.toggle('colorFull');
 }
 
+
+
+
+
+
+
+
+// let sWich = true;
+
+// btn.onclick = ()=>{
+//     if (sWich) {
+//     photo.attributes[1].value = "https://wallup.net/wp-content/uploads/2016/05/24/304149-nature.jpg"
+//         sWich = false;
+//     }else{
+//     photo.attributes[1].value = "https://wallpapercave.com/wp/wp5042743.jpg"
+//         sWich = true;
+
+//     }
+    
+// }
+
+// let switchTool = true;
+// btn.onclick = ()=>{
+//        if (switchTool) {
+//         photo[1].style.opacity = "0";
+//         photo[0].style.opacity = "1";
+//         switchTool = false;
+//        }else{
+//         photo[1].style.opacity = "1";
+//         photo[0].style.opacity = "0";
+//         switchTool = true
+//        }
+// }
 
 
 
