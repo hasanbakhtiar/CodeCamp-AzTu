11645d3cef784557ae03a2eb928942b663ed746b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-02-09 13:02:20 +0400

    project part 4

--- Project/assets/js/productcall.js
+++ Project/assets/js/productcall.js
@@ -5,13 +5,30 @@ fetch("https://fakestoreapi.com/products")
 .then(res=>res.json())
 .then(data=>{
 
-  console.log(data);
     let col = "";
-    let filterdata = data.filter(fd=>fd.category === decodeURI(location.href.slice(43,99)));
-    filterdata.map(i=>{
-      console.log(i);
+    if (location.href.slice(43,99)) {
+      let filterdata = data.filter(fd=>fd.category === decodeURI(location.href.slice(43,99)));
+      filterdata.map(i=>{
+          col+=`
+          <div data-aos="flip-up" class="col-12 col-sm-12 col-md-3" >
+          <div class="card" >
+            <img style="object-fit:contain" height="250" src="${i.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${i.title.slice(0,12)}...</h5>
+              <p class="card-text">${i.price}$</p>
+              <button type="button" id="basket-btn" class="btn btn-success">Add to card</button>
+            </div>
+          </div>
+        </div>
+          `
+       
+          
+        })
+    }else{
+
+      data.map(i=>{
         col+=`
-        <div class="col-12 col-sm-12 col-md-3" >
+        <div data-aos="flip-up" class="col-12 col-sm-12 col-md-3" >
         <div class="card" >
           <img style="object-fit:contain" height="250" src="${i.image}" class="card-img-top" alt="...">
           <div class="card-body">
@@ -26,8 +43,7 @@ fetch("https://fakestoreapi.com/products")
         
       })
 
+    }
     row.innerHTML = col;
 })
 
-
-console.log(window.location);
--- Project/assets/js/script.js
+++ Project/assets/js/script.js
@@ -2,8 +2,53 @@
 const header = document.querySelector('header');
 
 header.innerHTML = `
+<a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+Menu
+</a>
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div>
+      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+    </div>
+    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Products
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="products.html">All products</a></li>
+                    <li><a class="dropdown-item" href="products.html?=men's clothing">Men's clothing</a></li>
+                    <li><a class="dropdown-item" href="products.html?=women's clothing">Women's clothing</a></li>
+                    <li><a class="dropdown-item" href="products.html?=jewelery">Jewelery</a></li>
+                    <li><a class="dropdown-item" href="products.html?=electronics">Electronics</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="service.html">Services</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+        </ul>
+  </div>
+</div>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container">
+
     <a class="navbar-brand" href="home.html">Best Shop</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -21,6 +66,7 @@ header.innerHTML = `
                     Products
                 </a>
                 <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="products.html">All products</a></li>
                     <li><a class="dropdown-item" href="products.html?=men's clothing">Men's clothing</a></li>
                     <li><a class="dropdown-item" href="products.html?=women's clothing">Women's clothing</a></li>
                     <li><a class="dropdown-item" href="products.html?=jewelery">Jewelery</a></li>
@@ -41,7 +87,7 @@ header.innerHTML = `
         <div class="d-flex">
            ${localStorage.getItem('login') === "true" ? ` <a class="btn  me-3" href="#">${localStorage.getItem("fullname")}</a>
            <a class="btn btn-danger" id="logout">Log out</a>` : ` <a class="btn btn-outline-dark me-3" href="login.html">Login</a>
-           <a class="btn btn-warning" href="register.html">Register</a>`}
+           <a class="btn btn-warning animate__animated animate__infinite animate__slower animate__zoomInRight"  href="register.html">Register</a>`}
         </div>
     </div>
 </div>
@@ -78,5 +124,5 @@ footer.innerHTML = `
 
 
 
-
-
+// AOS start
+AOS.init();
